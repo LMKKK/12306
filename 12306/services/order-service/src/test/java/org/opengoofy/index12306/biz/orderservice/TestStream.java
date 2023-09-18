e8786b32a1684acd2039ddcadbc2530e2305fd8e
Mingkai Liu <1793645613l@gmail.com>
2023-09-18 16:38:22 +0800

    完善代码

---
+++ 12306/services/order-service/src/test/java/org/opengoofy/index12306/biz/orderservice/TestStream.java
@@ -0,0 +1,15 @@
+package org.opengoofy.index12306.biz.orderservice;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestStream {
+    @Test
+    public void testToString(){
+
+    }
+
+}
